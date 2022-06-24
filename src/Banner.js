3a64e8b022adf4bc3ffc7720f386e8d3de1eceaf
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-24 08:49:15 -0700

    adding search button on banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import { Button } from '@mui/material'
 
 const Banner = () => {
+  //use state show date calendar, not show is the current state
+  const [showSearch, setShowSearch] = useState(false)
+
   return (
     <div className="banner">
+      <div className="banner__search">
+        {showSearch && <h1>SHOW DATE PICKER</h1>}
+        <Button
+          //button clicked, it show dates (opposite of false = true)
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
+          Search Dates
+        </Button>
+      </div>
+      s
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
