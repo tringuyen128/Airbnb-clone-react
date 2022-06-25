bee1c7f836f54dcd2e4473213daf56d59a40b51e
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-24 20:18:47 -0700

    adding search page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import './App.css'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import SearchPage from './SearchPage'
 
 function App() {
   return (
     // BEM
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+
+        <Routes>
+          {/* render search page*/}
+          <Route path="/search" element={<SearchPage />} />
+
+          {/* render home page */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   )
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchPage.css'
+
+const SearchPage = () => {
+  return (
+    <div className="searchPage">
+      <h1>This is search page</h1>
+    </div>
+  )
+}
+
+export default SearchPage
